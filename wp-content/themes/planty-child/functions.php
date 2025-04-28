5305f79097ef6e702493a39a20d4c78ef7270b5a
raph-Lhmn <raphaellehmann69@gmail.com>
2025-04-28 08:35:45 +0200

    formulaire+admin

--- wp-content/themes/planty-child/functions.php
+++ wp-content/themes/planty-child/functions.php
@@ -15,3 +15,13 @@ add_action( 'wp_enqueue_scripts', 'planty_child_style' );
  * Your code goes below.
  */
 
+/*--------Admin--------*/
+function planty_admin_button_in_menu($items, $args) {
+    if (is_user_logged_in() && current_user_can('administrator') && $args->menu == 'main-menu') { 
+        // Vérifie si le menu est celui de l'en-tête (ajuste 'primary' si nécessaire)
+        $items .= '<li class="button_rencontrer menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-16 current_page_item parent hfe-creative-menu"><a href="' . admin_url() . '" class="admin-button">Admin</a></li>';
+    }
+    return $items;
+}
+add_filter('wp_nav_menu_items', 'planty_admin_button_in_menu', 10, 2);
+
